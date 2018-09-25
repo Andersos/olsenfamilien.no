d58a42f68331c0ceca18cfcc75213f29f3eefbd5
Anders Olsen Sandvik <288977+Andersos@users.noreply.github.com>
2018-09-25 21:20:37 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const nodestatic = require('node-static');
-const views = new nodestatic.Server('./views');
-
-require('http').createServer(function (request, response) {
-        request.addListener('end', function () {
-            views.serve(request, response);
-        }).resume();
-}).listen(process.env.PORT || 3000);
